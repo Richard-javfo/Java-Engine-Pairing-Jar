d635dacf76c025f4e716a2651db289764d7cb9d6
Richard-javfo <kaiser.richard@googlemail.com>
2026-05-03 22:58:13 +0200

    inerface angepasst TRF parser erstellt

--- src/trf/api/TournamentState.java
+++ src/trf/api/TournamentState.java
@@ -6,19 +6,11 @@ package trf.api;
 
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import trf.api.RankedPlayer.TiebreakEntry;
-import trf.impl.TournamentInfoImpl;
-import trf.impl.TournamentPlayerImpl;
 import trf.impl.tiebreak.TiebreakStrategy;
 
 /**
